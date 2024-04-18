b2397ed2f05774d40e04e23bf6ef36dcf98c5fe0
Alexis Jimenez <alexismjm041998@gmail.com>
2024-04-17 20:18:54 -0400

    updated FoodItem.h

--- src/FoodItem.h
+++ src/FoodItem.h
@@ -1,1 +1,38 @@
+#pragma once
+#include "NutritionFacts.h" 
+#include "RBT.h"
 
+class FoodItem {
+public:
+    string id;
+    string description;
+
+    int selectedQuantity;
+    string selectedWeight;
+
+
+    NutritionFacts nutrition;
+
+
+    std::map<std::string, double> nutrients;
+    map<string, pair<double, string>> servings;  // Key is serving description, value is weight and description
+
+
+    FoodItem(string id, string description, const NutritionFacts& nutrition)
+        : id(id), description(description), nutrition(nutrition) {}
+
+    void addNutrient(const std::string& nutrientCode, double amount) {
+        nutrients[nutrientCode] = amount;
+    }
+    void setSelectedQuantity(int quantity) {
+        selectedQuantity = quantity;
+    }
+
+    void setSelectedWeight(const string& weight) {
+        selectedWeight = weight;
+    }
+
+    void addServing(const string& description, double weight) {
+        servings[description] = make_pair(weight, description);
+    }
+};
